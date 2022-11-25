c35c0624fb37a284832cf3ec3503b26a6db76224
sauravhathi <61316762+sauravhathi@users.noreply.github.com>
2022-11-25 22:23:30 +0530

    published

--- README.md
+++ README.md
@@ -1,70 +1,55 @@
-# Getting Started with Create React App
+# ImageToPdf
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This app converts multiple images into a single pdf. It is built using React, TailwindCSS, and jsPDF.
 
-## Available Scripts
+## Demo
 
-In the project directory, you can run:
+https://sauravhathi.github.io/imagetopdf/
 
-### `npm start`
+## Features
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+- Convert multiple images into a single pdf
+- Download the pdf
+- View the pdf
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## useState
 
-### `npm test`
+- image: base64 encoded string of the image
+- download: base64 encoded string of the pdf
+- view: blob url of the pdf
+- isComplete: boolean that is used to trigger the download and view buttons
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Methods
 
-### `npm run build`
+- handleImage: called when the user selects a file
+- handleConvert: called when the user clicks the convert button
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## Dependencies
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+- jspdf
+- reactJs
+- tailwindcss
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+## Installation
 
-### `npm run eject`
+Clone the repository and install the dependencies.
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+```bash
+git clone https://github.com/sauravhathi/imagetopdf.git
+cd imagetopdf
+npm install
+```
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+## Usage
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+```bash
+npm start
+```
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+## Contributing
 
-## Learn More
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+## License
 
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+[MIT](https://choosealicense.com/licenses/mit/)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import { jsPDF } from "jspdf";
+import { useState } from 'react';
 
 function App() {
+  // image is a base64 encoded string
+  const [image, setImage] = useState(null);
+  // download is a boolean that is used to trigger the download
+  const [download, setDownload] = useState(null);
+  // view is a boolean that is used to trigger the view
+  const [view, setView] = useState(null);
+
+  const [isComplete, setIsComplete] = useState(false);
+
+  // handleFile is called when the user selects a file
+  const handleImage = (e) => {
+    const files = e.target.files;
+    const images = [];
+    // Loop through all the files
+    for (let i = 0; i < files.length; i++) {
+      images.push(URL.createObjectURL(files[i]));
+      console.log("Saurav Hathi");
+    }
+    // Set the image state
+    setImage(images);
+  };
+
+  // handleConvert is called when the user clicks the convert button
+  const handleConvert = () => {
+    // create a new jsPDF instance and set the page size to A4 portrait
+    const doc = new jsPDF("p", "mm", "a4");
+
+    // add the image to the document and set the position to the center of the page
+    const defaultWidth = 210;
+    const defaultHeight = 297;
+
+    image.forEach((img, index) => {
+      // get the width and height of the image
+      const imgWidth = doc.getImageProperties(img).width;
+      const imgHeight = doc.getImageProperties(img).height;
+      // cal ratio to scale the image
+      const ratio = imgWidth / imgHeight;
+      const width = defaultWidth;
+      const height = width / ratio;
+      // add the image to the document
+      doc.addImage(img, "JPEG", 0, 0, width, height);
+      // when the image is not the last one, add a new page
+      if (index < image.length - 1) doc.addPage();
+    }
+    );
+    // dataUrl is the base64 encoded string of the pdf
+    const pdf = doc.output("dataurlstring");
+    // set the download state to the dataUrl
+    setDownload(pdf);
+    // set the view state to the bloburl
+    setView(doc.output("bloburl"));
+    // set the isComplete state to true
+    setIsComplete(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container mx-auto">
+      <div
+        className="flex flex-col justify-center h-screen gap-4 w-9/12 mx-auto md:w-4/12"
+      >
+        <h1 className="text-2xl font-bold text-center">Image To Pdf</h1>
+        <p className="text-center">Convert your multiple images into a single pdf</p>
+        {isComplete ? (
+          <p className="text-center text-green-500">100% Complete number of pages: {image.length}</p>
+        ) : null}
+
+        <div className="flex flex-col gap-2">
+          <label
+            className="flex flex-col items-center px-4 py-20 bg-white text-blue-500 rounded-lg shadow-lg uppercase border border-blue-500 cursor-pointer hover:bg-slate-800 hover:text-white hover:border-slate-50"
+          >
+            <img src={require("./upload.png")} alt="upload"
+              className="w-10 h-10 text-slate-900" />
+            <span className="mt-2 text-base leading-normal">Select file</span>
+            <input id="file" type="file" className="hidden" multiple onChange={handleImage} accept="image/*" />
+          </label>
+        </div>
+        <div className="flex flex-col">
+          {image ? (
+            <button
+            id="convert"
+            className="bg-slate-50 text-lg font-bold text-slate-900 p-2 rounded"
+            onClick={handleConvert}
+          >
+            Convert
+          </button> 
+          ) : null}
+
+        </div>
+        {isComplete ? (
+          <div id="pdfViewAndDl">
+          <div className="flex flex-row items-center justify-between gap-2">
+            <a id="download" className="bg-slate-50 text-lg font-bold text-slate-900 p-2 rounded" href={download} download="imageToPdf.pdf">Download</a>
+            <a id="view" className="bg-slate-50 text-lg font-bold text-slate-900 p-2 rounded " href={view} target="_blank">View Pdf</a>
+          </div>
+        </div> 
+        ) : null}
+        <footer
+          className="text-center my-2 border-t-2 border-gray-200 pt-4"
+          id="checkAuthor"
         >
-          Learn React
-        </a>
-      </header>
+          <p className="text-sm">
+            Made with ❤️ by 
+            <a href="https://github.com/sauravhathi" className="font-semibold" target="_blank"> Saurav Hathi</a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
